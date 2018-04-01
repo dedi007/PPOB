ae6e013a26aa970574a6ac81a86ef869378d85c1
dedi007 <37971600+dedi007@users.noreply.github.com>
2018-04-01 13:42:32 +0700

    test

--- PPOB/Controllers/AccountController.cs
+++ PPOB/Controllers/AccountController.cs
@@ -14,6 +14,7 @@ namespace PPOB.Controllers
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
+        //test
         public AccountController()
         {
         }
